b8a326f2c11753ac5ea7fa99ab927862afb215c5
Rey <>
2020-05-13 14:43:52 -0600

    se agrego esa cosa

--- src/EjemploGitHub.java
+++ src/EjemploGitHub.java
@@ -6,6 +6,7 @@ public class EjemploGitHub {
     public static void main(String[] args) {
         System.out.println("Bienvenido...!");
         System.out.println("Hi");
+        System.out.println("otra cosa");
       
     }
     
